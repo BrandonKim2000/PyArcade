d267e3294c0697429425ea8690fd551fbbdc6263
Benjamin Beinish <benjaminbeinish@gmail.com>
2021-02-21 13:54:31 -0500

    Game fully running through proxy

--- pyarcade/__init__.py
+++ pyarcade/__init__.py
@@ -1,4 +1,4 @@
-from pyarcade.proxy import MastermindGameProxy
+from pyarcade.proxy import *
 from pyarcade.mastermind import MastermindGame
 from pyarcade.minesweeper import MinesweeperGame
 
@@ -27,16 +27,17 @@ def minesweeper():
     print("Press enter to start a new game of Minesweeper")
     input()
     game = MinesweeperGame()
+    proxy = MinesweeperGameProxy(game)
     d = {"game_id": 0}
     session = game.create_game(d)
     session_id = session["session_id"]
     game_in_session = True
     while (game_in_session):
-        game.read_game({"session_id": session_id})
+        proxy.read_game({"session_id": session_id})
         guess = input("Enter your guess as four integers separated by spaces\n")
         guess = guess.split(" ")
         guess = tuple([int(i) for i in guess])
-        res = game.update_game({"session_id": session_id, "guess": guess})
+        res = proxy.update_game({"session_id": session_id, "guess": guess})
         board = res["board"]
         for r in range(len(board)):
             line = ""
